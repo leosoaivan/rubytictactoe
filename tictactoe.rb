3043ba71dbb3b62d4b396a17be7ad3acfa60888f
Leonard Soai-Van <leo.soaivan@gmail.com>
2016-08-02 22:20:32 -0400

    Add draw & restart conditions

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,8 +1,6 @@
 class Game
-  attr_reader :player_one, :player_two
 
   def initialize
-    puts "Welcome to Ruby Tic-Tac-Toe!"
     @player_one = Player.new("Player 1", "X")
     @player_two = Player.new("Player 2", "O")
     @@turns = 0
@@ -10,25 +8,30 @@ class Game
   end
 
   def game_start
+    start_message
     @board = Board.new
     @board.print_board
     game_loop
+  end
+
+  def start_message
+    puts "Welcome to Ruby Tic-Tac-Toe!"
+    puts "Player 1 is \"X\", and Player 2 is \"O\"."
   end
 
   def game_loop
     begin
       place_marker
       @board.print_board
-    end until @@turns == 10 || @board.won_game?
-    puts "#{current_player.name} has won!"
-    @board.print_board
+    end until @@turns == 9 || @board.won_game? == TRUE
+    end_message
   end
 
   def place_marker
     @@turns += 1
     begin
       marker = choose_quadrant
-    end until @board.cell[marker][1] != ("X" || "O")
+    end until @board.cell[marker][1] != "X" && @board.cell[marker][1] != "O"
     @board.cell[marker] = [marker, "#{current_player.marker}"]
   end
 
@@ -43,6 +46,35 @@ class Game
   def current_player
     @@turns % 2 == 0 ? @player_two : @player_one
   end
+
+  def end_message
+  	if @board.won_game? == TRUE
+  	  puts " #{current_player.name} has won!"
+  	else
+  	  puts "The game has ended in a draw!"
+  	end
+    restart_query
+  end
+
+  def restart_query
+    puts "Would you like to play again? Y/N"
+    decision = gets.chomp.upcase
+    case decision
+    when "Y"
+      game_restart
+    when "N"
+      puts "Goodbye!"
+    else
+      puts "I didn't get that. Please try again."
+      restart_query
+    end
+  end
+
+  def game_restart
+  	@@turns = 0
+  	game_start
+  end
+
 end
 
 class Player
