5b2893865d354c6f7efc62463a41a2ed554e9507
Leonard <leo.soaivan@gmail.com>
2016-11-02 14:14:08 -0400

    Update

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,28 +2,52 @@ require './lib/game.rb'
 
 describe Game do
 
-  describe "#new" do
-    it "creates an instance of Game" do
-      player1 = double('player')
-      player2 = double('player')
+  let(:player_one) { double('player') }
+  let(:player_two) { double('player') }
+  let(:game) { Game.new(:player_one, :player_two) }
+  let(:board) { Board.new }
 
-      expect(Game.new(player1, player2)).to be_an_instance_of(Game)
+  describe "attributes" do
+    it "allows reading and writing for :player_one" do
+      game.player_one = "Leo"
+      expect(game.player_one).to eql("Leo")
     end
-
-    context "given two players" do
-      it "does not raise an ArgumentError" do
-        player1 = double('player')
-        player2 = double('player')
-        expect {Game.new(player1, player2)}.not_to raise_error
-      end
-    end
-
-    context "given one player" do
-      it "raises an ArgumentError" do
-        player1 = double('player')
-        expect {Game.new(player1)}.to raise_error(ArgumentError)
-      end
+    it "allows reading and writing for :turns" do
+      game.turns = 7
+      expect(game.turns).to eql(7)
     end
   end
+
+  # describe "#new" do
+  #   it "creates an instance of Game" do
+  #     expect(game).to be_an_instance_of(Game)
+  #   end
+  #
+  #   context "given two players" do
+  #     it "does not raise an ArgumentError" do
+  #       expect {game}.not_to raise_error
+  #     end
+  #   end
+  #
+  #   context "given one player" do
+  #     it "raises an ArgumentError" do
+  #       expect {Game.new(player1)}.to raise_error(ArgumentError)
+  #     end
+  #   end
+  # end
+  #
+  # describe "#game_start" do
+  #   it "displays a board" do
+  #     expect(board.print_board)
+  #   end
+  # end
+  #
+  # describe "#start_message" do
+  #   it "displays a welcome message" do
+  #     expect(game.start_message)
+  #   end
+  # end
+  #
+  # describe "#game_loop" do
 
 end
