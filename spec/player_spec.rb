b14f7c433e7b5c0f9e1e29ead26395a3b81856a4
Leonard <leo.soaivan@gmail.com>
2016-11-13 13:11:09 -0500

    Simplify Player test suite

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,28 +2,20 @@ require 'player.rb'
 
 describe Player do
 
+  describe "attributes" do
+    it "allows reading & writing to :name" do
+      subject.name = "Leo"
+      expect(subject.name).to eq("Leo")
+    end
+    it "allows reading & writing to :marker" do
+      subject.marker = "X"
+      expect(subject.marker).to eq("X")
+    end
+  end
+
   describe "#new" do
-    context "with two parameters" do
-      it "creates a new Player" do
-        expect(Player.new("Leo", "X")).to be_an_instance_of(Player)
-      end
-    end
-    context "with other number of parameters" do
-      it "returns an error" do
-        expect{ Player.new("Leo") }.to raise_error(ArgumentError)
-      end
-    end
-  end
-
-  describe "#name" do
-    it "returns Player's name" do
-      expect(Player.new("Leo", "X").name).to eql("Leo")
-    end
-  end
-
-  describe "#marker" do
-    it "returns Player's marker" do
-      expect(Player.new("Michiko", "O").marker).to eql("O")
+    it "creates an instance of Player" do
+      expect(subject).to be_an_instance_of(Player)
     end
   end
 
