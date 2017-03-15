894bcb795952c3278117df2509cdeeb6dca0bdde
L. Soai-Van <leo.soaivan@gmail.com>
2017-03-15 16:28:12 -0400

    Remove player spec

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,8 @@
 class Player
   attr_accessor :name, :marker
 
-  def initialize( options = {} )
-    self.name = options[:name]
-    self.marker = options[:marker]
+  def initialize(args = {})
+    self.name = args[:name]
+    self.marker = args[:marker]
   end
 end
